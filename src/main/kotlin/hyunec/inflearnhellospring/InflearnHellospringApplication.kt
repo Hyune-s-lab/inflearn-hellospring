16161b5e0e0aa078cb7607582596c6932fa87002
Hyune-c <choi8608@gmail.com>
2024-09-13 18:35:15 +0900

    project init

---
+++ src/main/kotlin/hyunec/inflearnhellospring/InflearnHellospringApplication.kt
@@ -0,0 +1,11 @@
+package hyunec.inflearnhellospring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnHellospringApplication
+
+fun main(args: Array<String>) {
+    runApplication<InflearnHellospringApplication>(*args)
+}
---
+++ src/test/kotlin/hyunec/inflearnhellospring/InflearnHellospringApplicationTests.kt
@@ -0,0 +1,13 @@
+package hyunec.inflearnhellospring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InflearnHellospringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
