8240f273ac2c4317350deb0bbabbd5f0058f8cbf
Hyune-c <choi8608@gmail.com>
2024-09-16 22:14:33 +0900

    [스프링 개발 시작하기] PaymentService 개발 (3)

---
+++ src/main/kotlin/hyunec/inflearnhellospring/payment/ExRateData.kt
@@ -0,0 +1,11 @@
+package hyunec.inflearnhellospring.payment
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.math.BigDecimal
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JvmRecord
+data class ExRateData(
+    val result: String,
+    val rates: Map<String, BigDecimal>,
+)
--- src/main/kotlin/hyunec/inflearnhellospring/payment/PaymentService.kt
+++ src/main/kotlin/hyunec/inflearnhellospring/payment/PaymentService.kt
@@ -1,17 +1,35 @@
 package hyunec.inflearnhellospring.payment
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.math.BigDecimal
+import java.net.HttpURLConnection
+import java.net.URL
 import java.time.LocalDateTime
+import java.util.stream.Collectors
 
 class PaymentService {
-    fun prepare(orderId: Long, currency: String, foreignCurrencyAmount: BigDecimal): Payment {
+    fun prepare(orderId: Long, currency: String, foreignCurrencyAmount: BigDecimal): Payment { // 환율 가져오기
         // 환율 가져오기
+        val url = URL("https://open.er-api.com/v6/latest/${currency}")
+        val connection = url.openConnection() as HttpURLConnection
+        val br = BufferedReader(InputStreamReader(connection.inputStream))
+        val response = br.lines().collect(Collectors.joining("\n"))
+        br.close()
+
+        val mapper = ObjectMapper()
+        val data = mapper.readValue(response, ExRateData::class.java)
+        val exRate = data.rates["KRW"]!!
+        println(exRate)
 
         // 금액 계산
+        val convertedAmount = foreignCurrencyAmount.multiply(exRate)
 
         // 유효 시간 계산
+        val validUntil = LocalDateTime.now().plusMinutes(30)
 
-        return Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now())
+        return Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil)
     }
 }
 
