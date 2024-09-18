7234103a90fe3cc5f4e3f1e3683ce017119cf1c8
Hyune-c <choi8608@gmail.com>
2024-09-18 20:33:09 +0900

    [오브젝트와 의존관계] 스프링 컨테이너와 의존관계 주입

--- src/main/kotlin/hyunec/inflearnhellospring/payment/Client.kt
+++ src/main/kotlin/hyunec/inflearnhellospring/payment/Client.kt
@@ -1,12 +1,14 @@
 package hyunec.inflearnhellospring.payment
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.math.BigDecimal
 
 class Client {}
 
 fun main(args: Array<String>) {
-    val objectFactory = ObjectFactory()
-    val paymentService = objectFactory.paymentService()
+    val beanFactory = AnnotationConfigApplicationContext(ObjectFactory::class.java)
+    val paymentService = beanFactory.getBean(PaymentService::class.java)
+
     val payment = paymentService.prepare(100, "USD", BigDecimal(50.7))
     println(payment)
 }
--- src/main/kotlin/hyunec/inflearnhellospring/payment/ObjectFactory.kt
+++ src/main/kotlin/hyunec/inflearnhellospring/payment/ObjectFactory.kt
@@ -1,10 +1,16 @@
 package hyunec.inflearnhellospring.payment
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class ObjectFactory {
+    @Bean
     fun paymentService(): PaymentService {
         return PaymentService(exRateProvider())
     }
 
+    @Bean
     fun exRateProvider(): ExRateProvider {
         return WebApiExRateProvider()
     }
